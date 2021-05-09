a869a5f1351dc6387cce785f57bc3f8bea235d83
irmakesin <esinirmak95@gmail.com>
2021-05-10 00:22:34 +0300

    propsCont

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00 AM" />
-      <CommentDetail author="Jane" timeAgo="Today at 5:00 PM" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" content="Nice blog!" avatar={faker.image.avatar()}/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:00 AM" content="I love it!" avatar={faker.image.avatar()}/>
+      <CommentDetail author="Jane" timeAgo="Today at 5:00 PM" content="Great blog!" avatar={faker.image.avatar()}/>
     </div>
   );
 };
