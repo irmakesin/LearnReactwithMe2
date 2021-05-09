ae36e65db132d96d205b6256d9f810a461391bad
irmakesin <esinirmak95@gmail.com>
2021-05-10 00:16:13 +0300

    multiple-props

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00 AM" />
+      <CommentDetail author="Jane" timeAgo="Today at 5:00 PM" />
     </div>
   );
 };
