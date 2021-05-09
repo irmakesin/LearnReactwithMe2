99ddbb26e1aaaedfd328b5e24c1aaa9fab8ad57a
irmakesin <esinirmak95@gmail.com>
2021-05-09 23:31:44 +0300

    faker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 if (module.hot) {
   module.hot.accept();
@@ -10,7 +11,7 @@ const App = () => {
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.image()} />
         </a>
         <div className="content">
           <a href="/" className="author">
